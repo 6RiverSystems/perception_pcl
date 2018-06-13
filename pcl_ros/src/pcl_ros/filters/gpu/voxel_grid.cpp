a24dc10254679487427a7714cf4ca1cf46b47257
zach <zachfang@6river.com>
2018-06-12 23:50:57 -0400

    chore: fix logging

--- pcl_ros/src/pcl_ros/filters/gpu/voxel_grid.cpp
+++ pcl_ros/src/pcl_ros/filters/gpu/voxel_grid.cpp
@@ -79,7 +79,7 @@ pcl_ros::gpu::VoxelGrid::config_callback (pcl_ros::VoxelGridConfig &config, uint
 
     if (config.leaf_size_x > 0 && config.leaf_size_y > 0 && config.leaf_size_z > 0)
     {
-        NODELET_WARN("pconfig_callback] All leaf values are set. Using the leaf_size_x, leaf_size_y, leaf_size_z values.");
+        NODELET_INFO("[config_callback] All leaf values are set. Using the leaf_size_x, leaf_size_y, leaf_size_z values.");
         leaf_size.setConstant (0);
         leaf_size[0] = config.leaf_size_x;
         leaf_size[1] = config.leaf_size_y;
@@ -87,7 +87,7 @@ pcl_ros::gpu::VoxelGrid::config_callback (pcl_ros::VoxelGridConfig &config, uint
     }
     else
     {
-        NODELET_ERROR("[config_callback] completely unexpected condition happened. Values are: leaf_size_y = %f, leaf_size_z = %f. Setting voxel grid size to 0.01.", config.leaf_size_x, config.leaf_size_y, config.leaf_size_z);
+        NODELET_ERROR("[config_callback] completely unexpected condition happened. Values are: leaf_size_x = %f, leaf_size_y = %f, leaf_size_z = %f. Setting voxel grid size to 0.01.", config.leaf_size_x, config.leaf_size_y, config.leaf_size_z);
         leaf_size = {0.01, 0.01, 0.01};
     }
     if (leaf_size != impl_.getLeafSize())
